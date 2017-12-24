67be2b2318883d9926613e0b8b4ca480566e7c8b
Michal Krzysztofowicz <mike@frozen-geek.com>
2017-12-24 18:22:27 +0000

    Refactor radarscoped.py to work as a daemon

--- radarscoped.py
+++ radarscoped.py
@@ -6,122 +6,461 @@ The receiver location is in the middle of the screen.
 
 """
 
+import argparse
+import atexit
+import grp
 import json
+import logging
+import logging.handlers
 import math
+import os
+import pwd
+import signal
+import sys
 import time
 import urllib.request
 
 import unicornhathd as uh
 
-def lon_length(latitude):
-    return 60 * math.cos(math.radians(latitude))
 
-def coord_span(radius, origin=(0, 0)):
-    lat = origin[0]
-    lon = origin[1]
+class Daemon(object):
+    """
+    A generic daemon class
 
-    lat_delta = float(radius/60.0)
-    lon_delta = float(radius/lon_length(lat))
+    Subclass the Daemon class and override the run() method
+    """
 
-    return {
-        "lat": {
-            "min": lat - lat_delta,
-            "max": lat + lat_delta,
-            "delta": lat_delta
-        },
-        "lon": {
-            "min": lon - lon_delta,
-            "max": lon + lon_delta,
-            "delta": lon_delta
+    def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null', daemon_name="Daemon"):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+        self.pidfile = pidfile
+        self.username = None
+        self.name = daemon_name
+        self.logger = logging.getLogger(self.name)
+        self.setup_logging()
+
+    def setup_logging(self):
+        self.logger.setLevel(logging.DEBUG)
+
+        logformatter = logging.Formatter('%(name)s: [%(levelname)s] %(message)s')
+
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.ERROR)
+        console_handler.setFormatter(logformatter)
+        self.logger.addHandler(console_handler)
+
+        syslog_handler = logging.handlers.SysLogHandler('/dev/log')
+        syslog_handler.setFormatter(logformatter)
+        self.logger.addHandler(syslog_handler)
+
+        # catch all unhandled exceptions
+        sys.excepthook = self.exception_log_handler
+
+    def exception_log_handler(self, type, value, tb):
+        self.logger.exception('Uncaught exception: {}'.format(str(value)))
+
+    def attach_stream(self, name, mode):
+        """
+        Replaces the stream with a new one
+        """
+        stream = open(getattr(self, name), mode)
+        os.dup2(stream.fileno(), getattr(sys, name).fileno())
+
+    def dettach_process(self):
+        self.fork()  # first fork, detach from parent
+
+        # Become a process group and session group leader
+        os.setsid()
+
+        # change to root directory
+        os.chdir('/')
+
+        self.fork()     # second fork, relinquish session leadership
+
+    # noinspection PyMethodMayBeStatic
+    def fork(self):
+        """
+        Spawn the child process
+        """
+        try:
+            pid = os.fork()
+            if pid > 0:
+                raise SystemExit(0)  # parent exits
+        except OSError as e:
+            self.logger.error("Fork failed: {} ({})".format(e.errno, e.strerror))
+            raise SystemExit(1)
+
+    def create_pidfile(self):
+        atexit.register(self.delete_pidfile)
+        pid = str(os.getpid())
+        open(self.pidfile, 'w+').write("{}\n".format(pid))
+
+    def delete_pidfile(self):
+        os.remove(self.pidfile)
+
+    # noinspection PyMethodMayBeStatic
+    def pid_exists(self, pid):
+        if pid < 0:
+            return False
+        try:
+            os.kill(pid, 0)
+        except ProcessLookupError:
+            return False
+        else:
+            return True
+
+    def get_pid(self):
+        try:
+            pf = open(self.pidfile, 'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except (IOError, TypeError):
+            pid = None
+        return pid
+
+    def status(self):
+        pid = self.get_pid()
+        if pid and self.pid_exists(pid):
+            message = "{} is running, pid: {}".format(self.name, pid)
+        else:
+            message = "{} not running".format(self.name)
+
+        return {
+            "message": message,
+            "pid": pid
         }
-    }
 
-def get_json(url):
-    request = urllib.request.urlopen(url)
-    data = request.read()
-    encoding = request.info().get_content_charset('utf-8')
-    json_data = json.loads(data.decode(encoding))
-    return json_data
+    def daemonize(self):
+        self.dettach_process()
 
-def get_origin():
-    data = get_json("http://piradar/dump1090-fa/data/receiver.json")
+        # Flush I/O buffers
+        sys.stdout.flush()
+        sys.stderr.flush()
 
-    latitude = data["lat"]
-    longitude = data["lon"]
-    return latitude, longitude
+        self.attach_stream('stdin', mode='r')
+        self.attach_stream('stdout', mode='a+')
+        self.attach_stream('stderr', mode='a+')
 
-def pixel_origin():
-    return 8, 8
+        self.create_pidfile()
 
-def get_aircraft():
-    data = get_json("http://piradar/dump1090-fa/data/aircraft.json")
-    return data["aircraft"]
+        # Setup signal handlers
+        signal.signal(signal.SIGINT, self.sigterm_handler)
+        signal.signal(signal.SIGQUIT, self.sigterm_handler)
+        signal.signal(signal.SIGTERM, self.sigterm_handler)
 
-def pixel_pos(radius, origin, position):
-    span = coord_span(radius, origin)
-    pixel_radius = 7
+        if self.username:
+            self.drop_privileges()
 
-    deg_per_px_lat = span["lat"]["delta"] / pixel_radius
-    deg_per_px_lon = span["lon"]["delta"] / pixel_radius
+    def start(self, username=None):
+        """
+        Start the daemon
+        """
 
-    delta_lat = position[0] - origin[0]
-    delta_lon = position[1] - origin[1]
+        self.logger.info("Starting.")
 
-    delta_x = delta_lon / deg_per_px_lon
-    delta_y = delta_lat / deg_per_px_lat
+        self.username = username
 
-    x_sign = 1
-    y_sign = 1
+        # check for a pid to see if the daemon is already running
+        pid = self.get_pid()
 
-    if origin[1] < 0:
-        x_sign = -1
-    if origin[0] < 0:
-        y_sign = -1
+        if pid:
+            if self.pid_exists(pid):
+                message = "pidfile {} already exists. {} already running?".format(self.pidfile, self.name)
+                self.logger.error(message)
+                raise SystemExit(1)
+            else:
+                message = "removing stale pid file"
+                self.logger.info(message)
+                self.delete_pidfile()
 
-    x = pixel_origin()[1] + (delta_x * x_sign)
-    y = pixel_origin()[0] + (delta_y * y_sign)
+        # Start the daemon
+        self.daemonize()
+        self.run()
 
-    if x < 0:
-        x = 0
-    if x > 15:
-        x = 15
+    def stop(self, silent=False):
+        """
+        Stop the daemon
+        """
 
-    if y < 0:
-        y = 0
-    if y > 15:
-        y = 15
+        # get the pid from the pidfile
+        pid = self.get_pid()
 
-    return int(x), int(y)
+        if not pid:
+            if not silent:
+                message = "pidfile {} does not exist. {} not running?".format(self.pidfile, self.name)
+                self.logger.info(message)
+            return  # not an error in a restart
+
+        # Try killing the daemon first
+        try:
+            while True:
+                os.kill(pid, signal.SIGTERM)
+                time.sleep(0.1)
+        except OSError as e:
+            e = str(e)
+            if e.find("No such process") > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+            else:
+                self.logger.error(e)
+                raise SystemExit(1)
+
+    def restart(self):
+        """
+        Restart the daemon
+        """
+        self.stop(silent=True)
+        self.start()
+
+    def drop_privileges(self):
+        """
+        Drop privileges if running as root
+        """
+
+        if os.getuid() != 0:
+            self.logger.info('drop_privileges: not running as root, nothing to do')
+            # we're not running as root, so nothing to do
+            return
+
+        try:
+            pwnam = pwd.getpwnam(self.username)
+            uid = pwnam.pw_uid
+
+        except Exception as e:
+            self.logger.error(str(e))
+            raise SystemExit(1)
+
+        # reset group privileges
+        try:
+            groups = [g.gr_gid for g in grp.getgrall() if self.username in g.gr_mem]
+            os.setgroups(groups)
+        except Exception as e:
+            self.logger.error(str(e))
+            raise SystemExit(1)
+
+        # try setting new uid
+        try:
+            os.setuid(uid)
+
+        except Exception as e:
+            self.logger.error(str(e))
+            raise SystemExit(1)
+
+        # ensure reasonable mask
+        os.umask(0o22)
+
+    # noinspection PyMethodMayBeStatic
+    def sigterm_handler(self, signo, frame):
+        self.logger.warning("Exiting.".format(signo))
+        raise SystemExit(1)
+
+    def run(self):
+        """
+        Override this method when subclassing Daemon
+        """
+        raise NotImplementedError
 
 
-def plot_positions(positions, radius):
-    origin = get_origin()
+class RadarDaemon(Daemon):
 
-    uh.off()
-    rcvr = pixel_origin()
-    uh.set_pixel(rcvr[0], rcvr[1], 255, 255, 255)
+    def __init__(self, pidfile, scope_radius,
+                 stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+        super().__init__(pidfile, stdin, stdout, stderr, daemon_name="radarscoped")
+        self.receiverurl = "http://piradar/dump1090-fa/data/receiver.json"
+        self.aircrafturl = "http://piradar/dump1090-fa/data/aircraft.json"
+        self.scope_radius = scope_radius
 
-    for position in positions:
-        pixel = pixel_pos(radius, origin, (position[0], position[1]))
-        print("aircraft: {}, {}".format(pixel[0], pixel[1]))
-        uh.set_pixel(pixel[0], pixel[1], 255, 128, 0)
+    # noinspection PyMethodMayBeStatic
+    def lon_length(self, latitude):
+        return 60 * math.cos(math.radians(latitude))
 
-    uh.show()
+    def coord_span(self, radius, origin=(0, 0)):
+        lat = origin[0]
+        lon = origin[1]
+
+        lat_delta = float(radius / 60.0)
+        lon_delta = float(radius / self.lon_length(lat))
+
+        return {
+            "lat": {
+                "min": lat - lat_delta,
+                "max": lat + lat_delta,
+                "delta": lat_delta
+            },
+            "lon": {
+                "min": lon - lon_delta,
+                "max": lon + lon_delta,
+                "delta": lon_delta
+            }
+        }
+
+    # noinspection PyMethodMayBeStatic
+    def get_json(self, url):
+        request = urllib.request.urlopen(url)
+        data = request.read()
+        encoding = request.info().get_content_charset('utf-8')
+        json_data = json.loads(data.decode(encoding))
+        return json_data
+
+    def get_origin(self):
+        data = self.get_json(self.receiverurl)
+
+        latitude = data["lat"]
+        longitude = data["lon"]
+        return latitude, longitude
+
+    # noinspection PyMethodMayBeStatic
+    def pixel_origin(self):
+        shape = uh.get_shape()
+        x = shape[0] / 2
+        y = shape[1] / 2
+        return x, y
+
+    # noinspection PyMethodMayBeStatic
+    def pixel_radius(self):
+        shape = uh.get_shape()
+        radius = math.floor(min(shape[0] / 2, shape[1] / 2))
+        return radius
+
+    def get_aircraft(self):
+        data = self.get_json(self.aircrafturl)
+        return data["aircraft"]
+
+    def pixel_pos(self, radius, origin, position):
+        span = self.coord_span(radius, origin)
+        shape = uh.get_shape()
+
+        deg_per_px_lat = span["lat"]["delta"] / self.pixel_radius()
+        deg_per_px_lon = span["lon"]["delta"] / self.pixel_radius()
+
+        delta_lat = position[0] - origin[0]
+        delta_lon = position[1] - origin[1]
+
+        delta_x = delta_lon / deg_per_px_lon
+        delta_y = delta_lat / deg_per_px_lat
+
+        x_sign = 1
+        y_sign = 1
+
+        if origin[1] < 0:
+            x_sign = -1
+        if origin[0] < 0:
+            y_sign = -1
+
+        x = self.pixel_origin()[1] + (delta_x * x_sign)
+        y = self.pixel_origin()[0] + (delta_y * y_sign)
+
+        if origin[1] < 0:
+            x = shape[0] - x
+        if origin[0] < 0:
+            y = shape[1] - y
+
+        if x < 0:
+            x = 0
+        if x > shape[0] - 1:
+            x = shape[0] - 1
+
+        if y < 0:
+            y = 0
+        if y > shape[1] - 1:
+            y = shape[1] - 1
+
+        return int(x), int(y)
+
+    # noinspection PyMethodMayBeStatic
+    def get_altitude_colour(self, altitude):
+
+        colour = (64, 64, 64)
+        if 0 < altitude <= 2000:
+            colour = (192, 128, 0)
+        elif 2000 < altitude <= 6000:
+            colour = (255, 128, 0)
+        elif 6000 < altitude <= 10000:
+            colour = (0, 255, 0)
+        elif 10000 < altitude <= 20000:
+            colour = (0, 255, 255)
+        elif 20000 < altitude <= 30000:
+            colour = (0, 0, 255)
+        elif altitude > 30000:
+            colour = (255, 0, 255)
+
+        return colour
+
+    def plot_positions(self, positions, radius):
+        origin = self.get_origin()
+
+        uh.off()
+
+        for position in positions:
+            pixel = self.pixel_pos(radius, origin, (position[0], position[1]))
+            colour = self.get_altitude_colour(position[2])
+            print("aircraft: {}, {}".format(pixel[0], pixel[1]))
+            uh.set_pixel(pixel[0], pixel[1], colour[0], colour[1], colour[2])
+
+        rcvr = self.pixel_origin()
+        uh.set_pixel(rcvr[0], rcvr[1], 255, 255, 255)
+        uh.show()
+
+    def run(self):
+        while True:
+            all_aircraft = self.get_aircraft()
+            ac_positions = list()
+            for plane in all_aircraft:
+                if "lat" in plane:
+                    lat = plane["lat"]
+                    lon = plane["lon"]
+                    if "altitude" in plane:
+                        alt = plane["altitude"]
+                    else:
+                        alt = 0
+                    ac_positions.append([lat, lon, alt])
+                    print("lat: {}, lon: {}, alt: {}".format(lat, lon, alt))
+
+            self.plot_positions(ac_positions, 72)
+            time.sleep(5)
+
 
 def main():
-    while True:
-        all_aircraft = get_aircraft()
-        ac_positions = list()
-        for plane in all_aircraft:
-            if "lat" in plane:
-                lat = plane["lat"]
-                lon = plane["lon"]
-                alt = plane["altitude"]
-                ac_positions.append([lat, lon, alt])
-                print("lat: {}, lon: {}, alt: {}".format(lat, lon, alt))
+    """
+    The application main entry point
+    """
 
-        plot_positions(ac_positions, 72)
-        time.sleep(5)
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(title='actions', dest='action', metavar='action')
+    parser_start = subparsers.add_parser('start', help='start radarscoped')
+    parser_start.add_argument('-u, --user', dest='username', help='user to run as', type=str, default=None)
+    parser_start.add_argument('-r, --radius', dest='radius',
+                              help='scope radius in Nautical Miles', type=int, default=72)
+
+    parser_stop = subparsers.add_parser('stop', help='stop radarscoped')
+    parser_restart = subparsers.add_parser('restart', help='restart radarscoped')
+    parser_status = subparsers.add_parser('status', help='get status for radarscoped')
+    subparsers.required = True
+
+    args = parser.parse_args()
+    action = args.action
+
+    # instantiate the daemon
+    radarscoped = RadarDaemon('/var/run/radarscoped.pid', args.radius)
+
+    if action == 'start':
+        radarscoped.start(username=args.username)
+        pid = radarscoped.get_pid()
+
+        if not pid:
+            print("Error starting radarscoped")
+
+    elif action == 'stop':
+        radarscoped.stop()
+
+    elif action == 'restart':
+        radarscoped.restart()
+
+    elif action == 'status':
+        radarscoped.status()
+
+    raise SystemExit(0)
 
 
 if __name__ == '__main__':
